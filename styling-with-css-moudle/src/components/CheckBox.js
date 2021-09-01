ecfd05cb1353e264d6e544f39ea1403d3f4305e4
YunPC <apsn2000@nate.com>
2021-09-01 11:15:04 +0900

    css modules 컴포넌트에 적용하기(#2)

--- styling-with-css-moudle/src/components/CheckBox.js
+++ styling-with-css-moudle/src/components/CheckBox.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md'
 import styles from './CheckBox.module.css'
+import classNames from 'classnames/bind'
 
-console.log(styles)
+const cx = classNames.bind(styles)
 
 function CheckBox({ checked, children, ...rest }) {
   return (
-    <div>
+    <div className={styles.checkbox}>
       <label>
         <input type="checkbox" checked={checked} {...rest} />
-        <div>{checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}</div>
+        <div className={styles.icon}>
+          {checked ? <MdCheckBox className={styles.checked} /> : <MdCheckBoxOutlineBlank />}
+        </div>
       </label>
       <span>{children}</span>
     </div>
