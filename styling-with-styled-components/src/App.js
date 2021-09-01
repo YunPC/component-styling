86d43651c772269cd0e6987db19d2020153268ab
YunPC <apsn2000@nate.com>
2021-09-01 12:56:04 +0900

    App 초기 상태로 복귀

--- styling-with-styled-components/src/App.js
+++ styling-with-styled-components/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
-
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background-color: black;
-  border-radius: 50%;
-`
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return <Circle />
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
 }
 
 export default App
